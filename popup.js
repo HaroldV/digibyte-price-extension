2220b5015e22a58f54369fbbd113eed97ca57bd9
hevillalobos <hevillalobos@ccxc.us>
2020-05-08 09:55:32 -0500

    [Add] V1.1

--- popup.js
+++ popup.js
@@ -1,8 +1,9 @@
 function getDigibytePrice() {        
-    fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=DGB&tsyms=USD')
+    fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=DGB&tsyms=USD,BTC')
     .then((res) => res.json())
     .then(function(data) {
-        document.getElementById('digi-price').append(`$${data.DGB.USD}`)
+        document.getElementById('digi-price-btc').append(`USD: ${data.DGB.USD}`)
+        document.getElementById('digi-price-usd').append(`BTC: ${data.DGB.BTC}`)
     });        
  }
 
